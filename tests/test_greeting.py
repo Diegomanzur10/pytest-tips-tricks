df9ad7fa35830fc6a36d43a73680d6dd35d2e3e2
Diego Manzur <43482953+Diegomanzur10@users.noreply.github.com>
2023-01-02 19:37:56 +0000

    adding another test layout

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,17 +1,4 @@
-import pytest
-
 from greeting import my_name
 
-@pytest.fixture
-def bob():
-    return "My name is: bob"
-
-@pytest.fixture
-def sally():
-    return "My name is: sally"
-
-def test_bob(bob):
-    assert bob == my_name("bob")
-
-def test_sally(sally):
-    assert sally == my_name("sally")
+def test_my_name():
+    assert my_name("Diego") == "My name is: Diego"
